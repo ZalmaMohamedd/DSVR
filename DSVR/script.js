674f015b1ab09d4833a2b0e08853cf1bf1982402
Salma Mohamed <73251390+ZalmaMohamedd@users.noreply.github.com>
2023-05-11 01:28:16 +0300

    Add files via upload

---
+++ DSVR/script.js
@@ -0,0 +1,43 @@
+searchForm = document.querySelector('#header .header-1 .search-form');
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+}     
+let loginForm = document.querySelector('#login .Login-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+  
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+window.onscroll = () =>{
+
+  searchForm.classList.remove('active');
+
+  if(window.scrollY > 80){
+      document.querySelector('#header .header-2').classList.add('active');
+  }else{
+      document.querySelector('#header .header-2').classList.remove('active');
+  }
+  
+}
+
+
+window.onload = () =>{
+
+  if(window.scrollY > 80){
+    document.querySelector('#header .header-2').classList.add('active');
+  }else{
+    document.querySelector('#header .header-2').classList.remove('active');
+  }
+
+   fadeOut(); 
+}
+
+
+
+
+
+
+
